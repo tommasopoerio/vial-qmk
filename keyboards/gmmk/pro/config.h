bf79234fa80d257c5b9d67784a2e900784cdb3bd
Tommaso Poerio <tommaso.poerio@eng.it>
2023-03-05 16:28:02 +0100

    set debounce to 8 ms to reduce chattering

--- keyboards/gmmk/pro/config.h
+++ keyboards/gmmk/pro/config.h
@@ -106,8 +106,8 @@
 #define ENABLE_RGB_MATRIX_SOLID_SPLASH
 #define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
 
-/* Set debounce time to 5ms */
-#define DEBOUNCE 5
+/* Set debounce time to 8ms to prevent chatter */
+#define DEBOUNCE 8
 
 /* Force NKRO on boot up regardless of the setting saved in the EEPROM (uncomment to enable it) */
 // #define FORCE_NKRO
