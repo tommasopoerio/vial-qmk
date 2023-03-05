a473fe11858b90cededac0162ccb8bfc0b61a058
Tommaso Poerio <tommaso.poerio@eng.it>
2023-03-05 16:29:46 +0100

    new profile based on gourdo1 - removed unused features like Paddle Game

---
+++ keyboards/gmmk/pro/rev1/iso/keymaps/brutalibus/rules.mk
@@ -0,0 +1,23 @@
+LTO_ENABLE = yes               # link time optimization -- achieves a smaller compiled size
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+
+VIAL_ENABLE=yes
+VIALRGB_ENABLE=yes
+
+
+VIA_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+BOOTMAGIC_ENABLE = yes         # Enable Bootmagic Lite
+CAPS_WORD_ENABLE = yes         # Enable built-in Caps Word functionality
+
+TD_LSFT_CAPSLOCK_ENABLE = yes
+IDLE_TIMEOUT_ENABLE = yes
+STARTUP_NUMLOCK_ON = yes
+ENCODER_DEFAULTACTIONS_ENABLE = no
+
+COLEMAK_LAYER_ENABLE = no     # Enable Colemak layer / set to no to disable
+INVERT_NUMLOCK_INDICATOR = yes
+
+
